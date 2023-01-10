1057cbc7cdd2709776ec7d959a654b5ead58fbe4
Morgan <Mharrisstoertz@gmail.com>
2023-01-10 12:15:10 -0500

    Initial Commit

---
+++ LayoutExercise/LayoutExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExerciseApp.swift
+//  LayoutExercise
+//
+//  Created by Morgan Harris-Stoertz on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
